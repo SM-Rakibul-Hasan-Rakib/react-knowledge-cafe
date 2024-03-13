9c77a3bf109f1bb17b7696d21b2fb1931399ad9d
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-03-13 15:24:19 +0600

    first commmit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Header from "./Components/Header/Header";
+// import Header from './Components/Header/Header'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Header></Header>
+    
+      {/* <h1 className="text-6xl">Knowledge Cafe</h1> */}
+    </>
+  );
+}
+
+export default App;
